ff11fc4896c105217deca948ea7f81469972108d
Beatriz Vieira <beaferreirav@gmail.com>
2023-10-24 17:40:44 +0100

    react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { quiz } from './questions.js'
+import './quizz.css'
 
-function App() {
+const Quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0)
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [showResult, setShowResult] = useState(false)
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  })
+
+  const { questions } = quiz
+  const { question, choices, correctAnswer } = questions[activeQuestion]
+
+  const onClickNext = () => {
+    setSelectedAnswerIndex(null)
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    )
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1)
+    } else {
+      setActiveQuestion(0)
+      setShowResult(true)
+    }
+  }
+
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index)
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true)
+    } else {
+      setSelectedAnswer(false)
+    }
+  }
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-container">
+      {!showResult ? (
+        <div>
+          <div className="quiz-questions">
+            <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
+            <span className="total-question">/{addLeadingZero(questions.length)}</span>
+          </div>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+                <li
+                    onClick={() => onAnswerSelected(answer, index)}
+                    key={answer}
+                    className={
+                        selectedAnswerIndex === index
+                        ? (selectedAnswer ? 'correct-answer' : 'incorrect-answer')
+                        : ''
+                    }
+                >
+                    {answer}
+                </li>
+            ))}
+          </ul>
+
+          <div className="flex-right">
+            <button onClick={onClickNext} disabled={selectedAnswerIndex === null}>
+              {activeQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Congrats!  You got {result.correctAnswers} question right out of {result.score} 😃!
+          </p>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default Quiz
---
+++ src/questions.js
@@ -0,0 +1,29 @@
+export const quiz = {
+  questions: [
+    {
+      question: "Who created React.js?",
+      choices: ["Jordan Walke","Jordan Mike", "Tim ford", "Rianne M"],
+      correctAnswer: "Jordan Walke"
+    },
+    {
+      question: "In which language is React.js written??",
+      choices: ["Ruby", "C#", "Javascript", "C++"],
+      correctAnswer: "Javascript"
+    },
+    {
+      question: "A state in React.js is also known as?",
+      choices: ["Internal storage of the component", "External storage of the component", "permanent storage"],
+      correctAnswer: "Internal storage of the component"
+    },
+    {
+      question: "In MVC, what does React.js act as?",
+      choices: ["Model", "View", "Controller"],
+      correctAnswer: "View"
+    },
+    {
+      question: "Which of the following are two ways to handle data in react?",
+      choices: ["States and Props", "Components and class", "Class and Props"],
+      correctAnswer: "States and Props"
+    }
+  ]
+}
